947d326d353c530b5828cd2fe7ba163fa0862b21
Борисов Кирилл <borisov@med.bars-open.ru>
2013-05-19 22:07:18 +0400

    Проверка ЭП сообщения.

--- libsmev/signer.py
+++ libsmev/signer.py
@@ -1,15 +1,47 @@
 #coding: utf-8
 
 import base64
+import os
+from tempfile import NamedTemporaryFile
 
 from lxml import etree
 
-from helpers import run_cmd, tags
+from helpers import run_cmd, tags, _from_soap
 from skeleton import make_node_with_ns
 from namespaces import NS_MAP
 
 
-def load_cert_from_pem(pem_filename):
+class SignerError(Exception):
+    pass
+
+
+def _format_pem(data):
+    u'''
+    Форматирование текстового представления сертификата
+    на линии по 64 символа и заключение его в теги для
+    передачи OpenSSL.
+
+    @param  data   Текст сертификата, закодированный в base64.
+    @type   unicode
+
+    @returns PEM, содержащий отформатированный сертификат.
+    @rtype    unicode
+    '''
+
+    result = []
+
+    data = data.replace('\n', '')
+    brackets = range(0, len(data) + 64, 64)
+
+    result.append('-----BEGIN CERTIFICATE-----')
+    for start, end in zip(brackets, brackets[1:]):
+        result.append(data[start:end])
+    result.append('-----END CERTIFICATE-----')
+
+    return '\n'.join(result)
+
+
+def load_cert_from_pem(data):
     u'''
     Загрузка данных публичного сертификата из PEM-контейнера
     (RFC 1421-1424).
@@ -21,10 +53,7 @@ def load_cert_from_pem(pem_filename):
     @rtype  unicode
     '''
 
-    data = None
-
-    with open(pem_filename, 'rb') as fp:
-        data = fp.read()
+    assert data, 'No PEM provided!'
 
     data = data.replace('\r', '').replace('\n', '')
     cert_start = data.find('-----BEGIN CERTIFICATE-----')
@@ -32,14 +61,38 @@ def load_cert_from_pem(pem_filename):
 
     # Не найдены маркеры начала и окончания сертификата в PEM
     if not (cert_start > -1 and cert_end > -1):
-        raise ValueError('PEM has no certificate markers (BEGIN, END)!')
+        raise SignerError('PEM has no certificate markers (BEGIN, END)!')
 
     return data[cert_start + 27:cert_end]
 
 
+def load_pubkey_from_pem(data):
+    u'''
+    Загрузка данных публичного ключа из PEM-контейнера
+    (RFC 1421-1424).
+
+    @param  pem_filename    Имя файла PEM-контейнера.
+    @type   pem_filename    unicode
+
+    @return base64-представление данных публичного ключа.
+    @rtype  unicode
+    '''
+
+    assert data, 'No PEM provided!'
+
+    load_key_cmd = ['openssl', 'x509', '-inform', 'PEM', '-pubkey', '-noout']
+
+    out, err = run_cmd(load_key_cmd, input=data)
+
+    if err:
+        raise SignerError(unicode(err))
+
+    return out
+
+
 def c14n_tags(tag):
     u'''
-    Исключительная каноникализация (см. http://www.w3.org/TR/xml-exc-c14n/) 
+    Исключительная каноникализация (см. http://www.w3.org/TR/xml-exc-c14n/)
     дерева XML-элементов.
 
     @param  tag     Корень дерева XML-элементов.
@@ -213,7 +266,8 @@ def sign_document(doc, priv_key_fn, priv_key_pass):
     security_node = tags(doc, '/SOAP-ENV:Envelope/SOAP-ENV:Header/wsse:Security')
 
     if not security_node:
-        cert_data = load_cert_from_pem(priv_key_fn)
+        with open(priv_key_fn, 'rb') as priv_key_file:
+            cert_data = load_cert_from_pem(priv_key_file.read())
         wsse_header_node = construct_wsse_header(certificate=cert_data)
         header_node[0].append(wsse_header_node)
 
@@ -232,3 +286,91 @@ def sign_document(doc, priv_key_fn, priv_key_pass):
     sign_val_node[0].text = signature_value
 
     return doc
+
+
+def verify_gost94_signature(text, public_key, signature_value):
+    u'''
+    Проверка корректности ЭП переданного текста по ГОСТ Р 34.11-94.
+
+    @param  text    Текст, подпись которого проверяется.
+    @type   text    unicode
+
+    @param  public_key  Публичный ключ, которым подписывался текст.
+    @type   public_key  unicode
+
+    @param  signature_value Подпись.
+    @type   signature_value unicode
+
+    @return Флаг корректности ЭП текста.
+    @type   boolean
+    '''
+
+    # Так как OpenSSL не умеет считывать значения подписи и проверяемый
+    # текст со стандартного ввода, мы вынуждены использовать временные файлы.
+    # После записи они закрываются, т.к. Windows не позволяет считывать
+    # открытые файлы.
+    tmp_public_key = NamedTemporaryFile(delete=False)
+    tmp_public_key.file.write(public_key)
+    tmp_public_key.file.close()
+
+    tmp_signature_value = NamedTemporaryFile(delete=False)
+    tmp_signature_value.file.write(base64.b64decode(signature_value))
+    tmp_signature_value.file.close()
+
+    openssl_sign_cmd = ['openssl', 'dgst', '-md_gost94', '-verify',
+                        tmp_public_key.name, '-signature',
+                        tmp_signature_value.name]
+
+    out, err = run_cmd(openssl_sign_cmd, input=text)
+
+    if err:
+        raise SignerError(unicode(err))
+
+    # Убираем за собой
+    os.remove(tmp_signature_value.name)
+    os.remove(tmp_public_key.name)
+
+    return out.strip() == "Verified OK"
+
+
+def verify_envelope_signature(envelope):
+    u'''
+    Проверка подписи SOAP-запроса по ГОСТ Р 34.11-94.
+
+    @param envelope     Подписанный XML-документ.
+    @type  envelope     lxml.Element
+
+    @return Флаг корректности подписи документа.
+    @rtype  boolean
+    '''
+
+    header, body = _from_soap(envelope)
+
+    if body is None:
+        raise SignerError("'Body' tag not found in SOAP envelope!'")
+
+    digest_value = tags(envelope, ".//ds:DigestValue")
+    if not digest_value:
+        raise SignerError("'DigestValue' tag is not found")
+
+    binary_security_token = tags(envelope, './/wsse:BinarySecurityToken')
+    if not binary_security_token:
+        raise SignerError("'BinarySecurityToken' tag is not found")
+
+    signed_info = tags(envelope, './/ds:SignedInfo')
+    if not signed_info:
+        raise SignerError("`SignedInfo' tag is not found")
+
+    signature_value = tags(envelope, ".//ds:SignatureValue")
+    if not signed_info:
+        raise SignerError("`SignatureValue' tag is not found")
+
+    if digest_value[0].text != get_text_digest(c14n_tags(body)):
+        return False
+
+    # Извлекаем публичный ключ из заголовка WS-Security
+    tmp_cert = _format_pem(binary_security_token[0].text)
+    public_key = load_pubkey_from_pem(tmp_cert)
+
+    return verify_gost94_signature(c14n_tags(signed_info[0]), public_key,
+                                   signature_value[0].text)
--- libsmev/tests.py
+++ libsmev/tests.py
@@ -1,10 +1,39 @@
+#coding: utf-8
+
 import unittest
+import os
 
 from lxml import etree
 
+from tempfile import NamedTemporaryFile
+
+from skeleton import construct_smev_envelope
 from helpers import dict_to_xmldoc
 from namespaces import NS_MAP
+from signer import sign_document, verify_envelope_signature
 
+# Тестовый ключ
+PEM = r'''
+-----BEGIN CERTIFICATE-----
+MIIBtzCCAWQCCQCUhYafJf4zaTAKBgYqhQMCAgMFADBiMQswCQYDVQQGEwJSVTES
+MBAGA1UECAwJVGF0YXJzdGFuMQ4wDAYDVQQHDAVLYXphbjETMBEGA1UECgwKQkFS
+Uy1Hcm91cDEMMAoGA1UECwwDRURVMQwwCgYDVQQDDANFRFUwHhcNMTIxMDIxMTAz
+MzQyWhcNMTMxMDIyMTAzMzQyWjBiMQswCQYDVQQGEwJSVTESMBAGA1UECAwJVGF0
+YXJzdGFuMQ4wDAYDVQQHDAVLYXphbjETMBEGA1UECgwKQkFSUy1Hcm91cDEMMAoG
+A1UECwwDRURVMQwwCgYDVQQDDANFRFUwYzAcBgYqhQMCAhMwEgYHKoUDAgIjAQYH
+KoUDAgIeAQNDAARAxdw/hP6gGRCqSYKMptrnObSGeu6OZ1Cc80WmbPR7S34hSvjG
+Ie2fEClMpDzBKZjuMsIijY3QxlIWyLZXYMX3bzAKBgYqhQMCAgMFAANBAMc+WaiP
+p8PLXZHAL6EgXDXomPIA7s97dHvPYqOlfuAmENZ/8yNvYN/VT4DdsqtRrZtMo0Og
+PmZZIIpBTbUSyvg=
+-----END CERTIFICATE-----
+
+-----BEGIN ENCRYPTED PRIVATE KEY-----
+MIGMMEAGCSqGSIb3DQEFDTAzMBsGCSqGSIb3DQEFDDAOBAg3P+ShojSyRAICCAAw
+FAYIKoZIhvcNAwcECLhovJf8tmC3BEhjHrDcLeg8vPnIMXdl8j4J6fsmcoSV9d7V
+MpXUYhi0SVVPA37O6LrLL6nqwRucIgZzVfpBKinmNvM9tdC4gVt+xepETiB1WsM=
+-----END ENCRYPTED PRIVATE KEY-----
+'''
+PEM_PASS = 'BARS_PASS_sidurniq38ymlskjn,jh<HGjhBJKGjmHGJKGM'
 
 class TestHelpersFunctions(unittest.TestCase):
     def setUp(self):
@@ -44,6 +73,96 @@ class TestHelpersFunctions(unittest.TestCase):
             assert inf_node, 'XPath "%s" returned no nodes!' % xpath
             self.assertEquals(inf_node[0].text, val)
 
+
+class TestSkeletonFunction(unittest.TestCase):
+    def setUp(self):
+        self.ctx = {
+            'Service': {
+                'Mnemonic': 'MONR001001',
+                'Version': '0.10'
+            },
+            'Sender': {
+                'Code': "SEND01001",
+                'Name': "Sender"
+            },
+            'Recipient': {
+                'Code': 'RECV01001',
+                'Name': 'Recipient'
+            },
+            'Originator': {
+                'Code': "SEND01001",
+                'Name': "Sender's origin"
+            },
+            'TypeCode': 'GSRV',
+            'Status': 'REQUEST',
+            'TestMsg': True
+        }
+
+        self.req = construct_smev_envelope('TestPacket', self.ctx)
+
+    def test_envelope_creation(self):        
+        prefix = './/SOAP-ENV:Body/inf:TestPacket/smev:Message'
+
+        paths_and_values = {
+            '%s/smev:Service/smev:Mnemonic' % prefix: self.ctx['Service']['Mnemonic'],
+            '%s/smev:Service/smev:Version' % prefix: self.ctx['Service']['Version'],
+            '%s/smev:Sender/smev:Code' % prefix: self.ctx['Sender']['Code'],
+            '%s/smev:Sender/smev:Name' % prefix: self.ctx['Sender']['Name'],
+            '%s/smev:Recipient/smev:Code' % prefix: self.ctx['Recipient']['Code'],
+            '%s/smev:Recipient/smev:Name' % prefix: self.ctx['Recipient']['Name'],
+            '%s/smev:Originator/smev:Code' % prefix: self.ctx['Originator']['Code'],
+            '%s/smev:Originator/smev:Name' % prefix: self.ctx['Originator']['Name'],
+            '%s/smev:TypeCode' % prefix: self.ctx['TypeCode'],
+            '%s/smev:Status' % prefix: self.ctx['Status'],
+            '%s/smev:TestMsg' % prefix: 'true',
+        }
+
+        for xpath, val in paths_and_values.items():
+            node = self.req.xpath(xpath, namespaces=NS_MAP)
+            assert node, 'XPath "%s" returned no nodes!' % xpath
+            self.assertEquals(node[0].text, val)
+
+
+class TestSigner(unittest.TestCase):
+    def setUp(self):
+        self.ctx = {
+            'Service': {
+                'Mnemonic': 'MONR001001',
+                'Version': '0.10'
+            },
+            'Sender': {
+                'Code': "SEND01001",
+                'Name': "Sender"
+            },
+            'Recipient': {
+                'Code': 'RECV01001',
+                'Name': 'Recipient'
+            },
+            'Originator': {
+                'Code': "SEND01001",
+                'Name': "Sender's origin"
+            },
+            'TypeCode': 'GSRV',
+            'Status': 'REQUEST',
+            'TestMsg': True
+        }
+
+        self.req = construct_smev_envelope('TestPacket', self.ctx)
+        self.tmp_file = NamedTemporaryFile(delete=False)
+        self.tmp_file.write(PEM)
+        self.tmp_file.close()
+
+    def test_sign_verify(self):
+        signed = sign_document(self.req, self.tmp_file.name, PEM_PASS)
+        assert verify_envelope_signature(signed), 'Signing mechanism produced invalid signature :('
+
+        sender_node = signed.xpath('.//SOAP-ENV:Body/inf:TestPacket/smev:Message/smev:Sender/smev:Name', namespaces=NS_MAP)[0]
+        sender_node.text = 'Impersonator'
+        assert not verify_envelope_signature(signed), 'Document was changed, but signature still verifies!'
+
+
+    def tearDown(self):
+        os.remove(self.tmp_file.name)
 
 if __name__ == '__main__':
     unittest.main()
