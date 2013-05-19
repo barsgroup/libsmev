8fb707dd7e5fdd0808eb6765ed512fa51d390046
Борисов Кирилл <borisov@med.bars-open.ru>
2013-05-19 17:40:48 +0400

    Изменена версия в setup.py, добавлена история изменений.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fn):
 
 setup(
     name='libsmev',
-    version='0.1.0',
+    version='0.1.1',
     author='Borisov Kirill',
     author_email='borisov@bars-open.ru',
     description=("Library of low-level helpers that make integration with SMEV"
