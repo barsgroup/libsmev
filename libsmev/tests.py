2c5e6520db2848104db29d318cf6ea6a568b3e77
Борисов Кирилл <borisov@bars-open.ru>
2014-02-23 16:25:26 +0400

    Добавлен тест на регрессию функции extract_smev_parts, образец правильного сообщения СМЭВ.

--- libsmev/tests.py
+++ libsmev/tests.py
@@ -13,7 +13,7 @@ from mimetypes import types_map
 from tempfile import NamedTemporaryFile, mkdtemp
 
 from skeleton import construct_smev_envelope
-from helpers import dict_to_xmldoc
+from helpers import dict_to_xmldoc, extract_smev_parts
 from namespaces import NS_MAP
 from signer import sign_document, verify_envelope_signature, get_text_digest
 from attachments import encode_directory, extract_directory
@@ -41,9 +41,14 @@ MpXUYhi0SVVPA37O6LrLL6nqwRucIgZzVfpBKinmNvM9tdC4gVt+xepETiB1WsM=
 '''
 PEM_PASS = 'BARS_PASS_sidurniq38ymlskjn,jh<HGjhBJKGjmHGJKGM'
 
+TEST_ENVELOPE = r'''<?xml version='1.0' encoding='utf-8'?>
+<SOAP-ENV:Envelope xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:ws="http://pe.minregion.ru/" xmlns:smev="http://smev.gosuslugi.ru/rev120315" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" xmlns:inf="http://smev.gosuslugi.ru/inf/" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"><SOAP-ENV:Header><wsse:Security SOAP-ENV:actor="http://smev.gosuslugi.ru/actors/smev"><wsse:BinarySecurityToken EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary" ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3" wsu:Id="CertId">MIIBtzCCAWQCCQCUhYafJf4zaTAKBgYqhQMCAgMFADBiMQswCQYDVQQGEwJSVTESMBAGA1UECAwJVGF0YXJzdGFuMQ4wDAYDVQQHDAVLYXphbjETMBEGA1UECgwKQkFSUy1Hcm91cDEMMAoGA1UECwwDRURVMQwwCgYDVQQDDANFRFUwHhcNMTIxMDIxMTAzMzQyWhcNMTMxMDIyMTAzMzQyWjBiMQswCQYDVQQGEwJSVTESMBAGA1UECAwJVGF0YXJzdGFuMQ4wDAYDVQQHDAVLYXphbjETMBEGA1UECgwKQkFSUy1Hcm91cDEMMAoGA1UECwwDRURVMQwwCgYDVQQDDANFRFUwYzAcBgYqhQMCAhMwEgYHKoUDAgIjAQYHKoUDAgIeAQNDAARAxdw/hP6gGRCqSYKMptrnObSGeu6OZ1Cc80WmbPR7S34hSvjGIe2fEClMpDzBKZjuMsIijY3QxlIWyLZXYMX3bzAKBgYqhQMCAgMFAANBAMc+WaiPp8PLXZHAL6EgXDXomPIA7s97dHvPYqOlfuAmENZ/8yNvYN/VT4DdsqtRrZtMo0OgPmZZIIpBTbUSyvg=</wsse:BinarySecurityToken><ds:Signature><ds:SignedInfo><ds:CanonicalizationMethod Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/><ds:SignatureMethod Algorithm="http://www.w3.org/2001/04/xmldsig-more#gostr34102001-gostr3411"/><ds:Reference URI="#body"><ds:Transforms><ds:Transform Algorithm="http://www.w3.org/2000/09/xmldsig#enveloped-signature"/><ds:Transform Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/></ds:Transforms><ds:DigestMethod Algorithm="http://www.w3.org/2001/04/xmldsig-more#gostr3411"/><ds:DigestValue>y1Feix2ktiF64VtgPmEyBtam5yaxkJeGdTcX3bg44h0=</ds:DigestValue></ds:Reference></ds:SignedInfo><ds:SignatureValue>5OyGOTSI0Nu+unDVU1c9jDn6fHIqILyKP8QJMefyqJ6zt7Oqxn+ws4wCkAU+FiNeBbPvty433CMPnOcSP6bgBg==</ds:SignatureValue><ds:KeyInfo><wsse:SecurityTokenReference><wsse:Reference URI="#CertId" ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3"/></wsse:SecurityTokenReference></ds:KeyInfo></ds:Signature></wsse:Security></SOAP-ENV:Header><SOAP-ENV:Body wsu:Id="body"><inf:TestRequest><smev:Message><smev:Sender><smev:Code>AAAA11112</smev:Code><smev:Name>Sender</smev:Name></smev:Sender><smev:Recipient><smev:Code>BBBB22222</smev:Code><smev:Name>Recipient</smev:Name></smev:Recipient><smev:Originator><smev:Code>AAAA11112</smev:Code><smev:Name>Originator</smev:Name></smev:Originator><smev:Service><smev:Mnemonic>TEST001001</smev:Mnemonic><smev:Version>0.10</smev:Version></smev:Service><smev:TypeCode>GSRV</smev:TypeCode><smev:Status>REQUEST</smev:Status><smev:Date>2014-02-23T11:54:38.8091</smev:Date><smev:ExchangeType>0</smev:ExchangeType><smev:TestMsg>true</smev:TestMsg></smev:Message><smev:MessageData><smev:AppData wsu:Id="AppData"><inf:Phrases><inf:Greeting><inf:Hello>World</inf:Hello></inf:Greeting></inf:Phrases></smev:AppData><smev:AppDocument><smev:RequestCode/><smev:BinaryData/></smev:AppDocument></smev:MessageData></inf:TestRequest></SOAP-ENV:Body></SOAP-ENV:Envelope>
+'''
+
 class TestHelpersFunctions(unittest.TestCase):
     def setUp(self):
         self.empty_doc = etree.Element('Empty')
+        self.envelope = etree.fromstring(TEST_ENVELOPE)
 
     def test_dict_to_xmldoc(self):
         test_dict = {
@@ -80,6 +85,20 @@ class TestHelpersFunctions(unittest.TestCase):
             self.assertEquals(inf_node[0].text, val)
 
 
+    def test_extract_smev_parts(self):
+        parts = extract_smev_parts(self.envelope)
+        assert len(parts) == 4, "Too many or too few parts returned."
+
+        tags = (
+            '{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd}BinarySecurityToken',
+            '{http://www.w3.org/2000/09/xmldsig#}Signature',
+            '{http://smev.gosuslugi.ru/rev120315}Message',
+            '{http://smev.gosuslugi.ru/rev120315}MessageData'
+        )
+        for (part, full_tag) in zip(parts, tags):
+            assert part.tag == full_tag, "{0} != {1}".format(part, full_part)
+
+
 class TestSkeletonFunction(unittest.TestCase):
     def setUp(self):
         self.ctx = {
@@ -106,7 +125,7 @@ class TestSkeletonFunction(unittest.TestCase):
 
         self.req = construct_smev_envelope('TestPacket', self.ctx)
 
-    def test_envelope_creation(self):        
+    def test_envelope_creation(self):
         prefix = './/SOAP-ENV:Body/inf:TestPacket/smev:Message'
 
         paths_and_values = {
@@ -176,8 +195,8 @@ class TestAttachments(unittest.TestCase):
         self.directory = mkdtemp()
         self.files = ['%s%s' % (str(uuid.uuid4()), ext) for ext in types_map.keys()]
 
-        # Файл без расширения, по которому можно было бы определить MIME-тип        
-        
+        # Файл без расширения, по которому можно было бы определить MIME-тип
+
         self.example_text = str(uuid.uuid4())
         self.example_hash = get_text_digest(self.example_text)
         self.example_sig_hash = get_text_digest(self.example_hash)
@@ -186,7 +205,7 @@ class TestAttachments(unittest.TestCase):
             with open(os.path.join(self.directory, fn), 'w') as f:
                 f.write(self.example_text)
 
-    def testEncode(self):        
+    def testEncode(self):
         req_code, encoded_zip = encode_directory(self.directory)
         decoded_zip = base64.b64decode(encoded_zip)
         sio = StringIO.StringIO(decoded_zip)
